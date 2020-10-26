090e914986163cede4bd6cab39b1bb340a23ce64
nathmotion <nmallet@excilys.com>
2020-10-26 11:40:12 +0100

    refactor: update version vue + change shims + use BEM naming for class name and css

--- src/components/Tasks/TaskForm.vue
+++ src/components/Tasks/TaskForm.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="task-form">
-    <form class="form" @submit.prevent="handleSubmit">
-      <input class="input" type="text" v-model="task.label" placeholder="New task" />
-      <i class="material-icons button-add" @click="handleSubmit">post_addgi</i>
+  <div class="task-layout__task-form">
+    <form class="task-form__form" @submit.prevent="handleSubmit">
+      <input class="form__input" type="text" v-model="task.label" placeholder="New task" />
+      <i class="material-icons form__button-add" @click="handleSubmit">post_addgi</i>
     </form>
-    <p v-if="errorEmptyLabel" class="error-message">Please enter a valid label.</p>
+    <p v-if="errorEmptyLabel" class="form__error-message">Please enter a valid label.</p>
   </div>
 </template>
 
@@ -42,39 +42,39 @@ export default TaskForm
 </script>
 
 <style scoped>
-.task-form {
+.task-layout__task-form {
   margin-bottom: 3em;
 }
 
-.form {
+.task-form__form {
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-.button-add {
+.form__button-add {
   align-content: center;
   cursor: pointer;
   margin-left: var(--space-sm);
   font-size: var(--font-size-lg);
 }
 
-.button-add:hover {
+.form__button-add:hover {
   color: black;
 }
 
-.input {
+.form__input {
   background-color: #bfbebb;
   border: none;
   border-radius: var(--border-radius-input);
   padding: var(--space-sm);
 }
 
-.input:focus {
+.form__input:focus {
   outline: none;
 }
 
-.error-message {
+.form__error-message {
   color: red;
   font-size: 75%;
   position: absolute;
