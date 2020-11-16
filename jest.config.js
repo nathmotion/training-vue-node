e06747b15fbdad29df1cfbcda6659fc6516ac3ee
nathmotion <nmallet@excilys.com>
2020-11-16 14:01:47 +0100

    wip: change vesion vue-jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '^.+\\.(vue)$': 'vue-jest'
+    '^.+\\.vue$': 'vue-jest'
   },
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
   testEnvironment: 'node'
