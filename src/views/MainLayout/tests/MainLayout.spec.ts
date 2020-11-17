3effdf705cacdb38bf243537e80a3e71d9aff36d
Tanguy Chusseau <tchusseau@excilys.com>
2020-11-17 15:24:35 +0100

    feat: unit test on MainLayout.vue

--- src/views/MainLayout/tests/MainLayout.spec.ts
+++ src/views/MainLayout/tests/MainLayout.spec.ts
@@ -1,9 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
 import MainLayout from '../MainLayout.vue'
+import TaskLayout from '../../Tasks/TaskLayout.vue'
 
 describe('MainLayout', () => {
-  it('renders a div', () => {
+  it('renders child component TaskLayout', () => {
+    // Given
     const wrapper = shallowMount(MainLayout)
-    console.info('rendu : ', wrapper.html())
+    // When
+    const childComponents = wrapper.findAllComponents(TaskLayout)
+    const taskLayout = wrapper.findComponent(TaskLayout)
+    // Then
+    expect(childComponents).toHaveLength(1)
+    expect(taskLayout.exists()).toBe(true)
   })
 })
